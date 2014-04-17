9e5533c2e4a8e8209412264f613302bd4954a08e
Michele Nasti <michele.nasti@gmail.com>
2014-04-17 20:19:41 +0200

    added upload and start actions

--- mybox-server/server.js
+++ mybox-server/server.js
@@ -3,16 +3,13 @@ var url = require("url");
 
 var PORT = 8888; 
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(pathname);
+    route(handle, pathname, response);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
   }
 
   http.createServer(onRequest).listen(PORT);
